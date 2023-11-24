3a67e73a2ab9545b0f5f9830aabdb5f3456e2ecc
carlospuigserver <cpuiggut@myuax.com>
2023-11-24 17:26:55 +0100

    c

--- calculadora/core/views.py
+++
@@ -1,8 +0,0 @@
-
-
-# Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def calculadora_view(request):
-    return render(request, 'calculadora/calculadora.html')

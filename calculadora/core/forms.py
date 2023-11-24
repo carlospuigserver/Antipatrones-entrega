059d8f7458ef8ef7c537050c7a6d95d04191e4d6
carlospuigserver <cpuiggut@myuax.com>
2023-11-24 18:13:04 +0100

    comit

--- calculadora/core/forms.py
+++
@@ -1,5 +0,0 @@
-# core/forms.py
-from django import forms
-
-class CalculatorForm(forms.Form):
-    expression = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly', 'class': 'form-control', 'id': 'expression'}))
--- calculadora/core/views.py
+++ calculadora/core/views.py
@@ -1,18 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import CalculatorForm
+from django.shortcuts import render,HttpResponse
 
-def calculator(request):
-    form = CalculatorForm()
-
-    if request.method == 'POST':
-        form = CalculatorForm(request.POST)
-        if form.is_valid():
-            expression = form.cleaned_data['expression']
-            try:
-                result = eval(expression)
-                form.cleaned_data['expression'] = str(result)
-            except Exception as e:
-                form.add_error('expression', f"Error en la expresión: {str(e)}")
-
-    return render(request, 'core/calculator.html', {'form': form})
+def home(request):
+ return HttpResponse("<h1>Mi Web Personal</h1><h2>Portada</h2>")
